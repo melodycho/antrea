ffbb9559d1e1a63e0bca4eba1a10fb2b7b95539f
Wenqi Qiu <wenqiq@vmware.com>
2024-02-23 13:25:38 +0800

    Update service scale up
    Signed-off-by: Wenqi Qiu <wenqiq@vmware.com>

--- test/performance/framework/service/scale_up.go
+++ test/performance/framework/service/scale_up.go
@@ -18,6 +18,7 @@ import (
 	"context"
 	"fmt"
 	"k8s.io/client-go/rest"
+	"strconv"
 	"time"
 
 	"github.com/google/uuid"
@@ -27,8 +28,11 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/klog/v2"
 
+	"antrea.io/antrea/test/performance/framework/workload_pod"
 	"antrea.io/antrea/test/performance/utils"
 )
+
+var startSvcCIDR = "10.100.100."
 
 func generateService(ns string, num int) (svcs []*corev1.Service) {
 	for i := 0; i < num; i++ {
@@ -60,9 +64,67 @@ type ServiceInfo struct {
 	NameSpace string
 }
 
+// func retrieveCIDRs(cmd string, reg string) ([]string, error) {
+// 	res := make([]string, 2)
+// 	rc, stdout, _, err := data.RunCommandOnNode(controlPlaneNodeName(), cmd)
+// 	if err != nil || rc != 0 {
+// 		return res, fmt.Errorf("error when running the following command `%s` on control-plane Node: %v, %s", cmd, err, stdout)
+// 	}
+// 	re := regexp.MustCompile(reg)
+// 	matches := re.FindStringSubmatch(stdout)
+// 	if len(matches) == 0 {
+// 		return res, fmt.Errorf("cannot retrieve CIDR, unexpected kubectl output: %s", stdout)
+// 	}
+// 	cidrs := strings.Split(matches[1], ",")
+// 	if len(cidrs) == 1 {
+// 		_, cidr, err := net.ParseCIDR(cidrs[0])
+// 		if err != nil {
+// 			return res, fmt.Errorf("CIDR cannot be parsed: %s", cidrs[0])
+// 		}
+// 		if cidr.IP.To4() != nil {
+// 			res[0] = cidrs[0]
+// 		} else {
+// 			res[1] = cidrs[0]
+// 		}
+// 	} else if len(cidrs) == 2 {
+// 		_, cidr, err := net.ParseCIDR(cidrs[0])
+// 		if err != nil {
+// 			return res, fmt.Errorf("CIDR cannot be parsed: %s", cidrs[0])
+// 		}
+// 		if cidr.IP.To4() != nil {
+// 			res[0] = cidrs[0]
+// 			res[1] = cidrs[1]
+// 		} else {
+// 			res[0] = cidrs[1]
+// 			res[1] = cidrs[0]
+// 		}
+// 	} else {
+// 		return res, fmt.Errorf("unexpected cluster CIDR: %s", matches[1])
+// 	}
+// 	return res, nil
+// }
+
 func ScaleUp(ctx context.Context, kubeConfig *rest.Config, cs kubernetes.Interface, nss []string, numPerNs int, ipv6 bool, maxCheckNum int, ch chan time.Duration, clientPods []corev1.Pod) (svcs []ServiceInfo, actualCheckNum int, err error) {
 	start := time.Now()
-	for _, ns := range nss {
+
+	// svcCIDRs, err := retrieveCIDRs("kubectl cluster-info dump | grep service-cluster-ip-range", `service-cluster-ip-range=([^"]+)`)
+	// if err != nil {
+	// 	// Retrieve service CIDRs for Rancher clusters.
+	// 	svcCIDRs, err = retrieveCIDRs("ps aux | grep kube-controller | grep service-cluster-ip-range", `service-cluster-ip-range=([^\s]+)`)
+	// 	if err != nil {
+	// 		return err
+	// 	}
+	// }
+
+	for i, ns := range nss {
+		var podList *corev1.PodList
+		podList, err = cs.CoreV1().Pods(ns).List(ctx, metav1.ListOptions{})
+		if err != nil {
+			return
+		}
+
+		fromPod := podList.Items[0]
+
 		klog.InfoS("Scale up Services", "Namespace", ns)
 		for _, svc := range generateService(ns, numPerNs) {
 			if ipv6 {
@@ -70,6 +132,13 @@ func ScaleUp(ctx context.Context, kubeConfig *rest.Config, cs kubernetes.Interfa
 				svc.Spec.IPFamilies = []corev1.IPFamily{ipFamily}
 			}
 			if err := utils.DefaultRetry(func() error {
+
+				clusterIP := startSvcCIDR + strconv.Itoa(i+1)
+				if err = workload_pod.Update(ctx, cs, fromPod.Namespace, fromPod.Name, []string{fmt.Sprintf("%s:%d", clusterIP, 80)}, workload_pod.ScaleClientPodProbeContainerName); err != nil {
+					klog.ErrorS(err, "Update test Pod failed")
+				}
+				klog.InfoS("Update test Pod to check Service", "ClusterIP", clusterIP)
+
 				var newSvc *corev1.Service
 				var err error
 				newSvc, err = cs.CoreV1().Services(ns).Create(ctx, svc, metav1.CreateOptions{})
