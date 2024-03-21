624fc4164b820ba891891da72ee0efc659e1e874
Wenqi Qiu <wenqiq@vmware.com>
2024-03-21 18:23:43 +0800

    Update
    Signed-off-by: Wenqi Qiu <wenqiq@vmware.com>

--- test/performance/framework/utils/praser.go
+++ test/performance/framework/utils/praser.go
@@ -3,12 +3,15 @@ package utils
 import (
 	"fmt"
 	"io/ioutil"
+	"k8s.io/klog/v2"
+
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/serializer/yaml"
 )
 
 func ReadYamlFile(yamlFile string) (runtime.Object, error) {
+	klog.InfoS("ReadYamlFile", "yamlFile", yamlFile)
 	podBytes, err := ioutil.ReadFile(yamlFile)
 	if err != nil {
 		return nil, fmt.Errorf("error reading YAML file: %+v", err)
